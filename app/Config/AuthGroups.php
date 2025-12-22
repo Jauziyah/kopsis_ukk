b805aef1c9aae71cc2d7aba1b657454eac130673
Jauziyah <mejaghani@gmail.com>
2025-12-22 14:01:04 +0700

    initial file configuration

--- app/Config/AuthGroups.php
+++ app/Config/AuthGroups.php
@@ -41,26 +41,18 @@ class AuthGroups extends ShieldAuthGroups
      * @see https://codeigniter4.github.io/shield/quick_start_guide/using_authorization/#change-available-groups for more info
      */
     public array $groups = [
-        'superadmin' => [
-            'title'       => 'Super Admin',
-            'description' => 'Complete control of the site.',
-        ],
         'admin' => [
             'title'       => 'Admin',
             'description' => 'Day to day administrators of the site.',
         ],
-        'developer' => [
-            'title'       => 'Developer',
-            'description' => 'Site programmers.',
+        'guru' => [
+            'title'       => 'guru',
+            'description' => 'guru pengawas.',
         ],
-        'user' => [
-            'title'       => 'User',
-            'description' => 'General users of the site. Often customers.',
-        ],
-        'beta' => [
-            'title'       => 'Beta User',
-            'description' => 'Has access to beta-level features.',
-        ],
+        'siswa' => [
+            'title'       => 'siswa',
+            'description' => 'General users of the site.',
+        ]
     ];
 
     /**
@@ -72,13 +64,13 @@ class AuthGroups extends ShieldAuthGroups
      * If a permission is not listed here it cannot be used.
      */
     public array $permissions = [
-        'admin.access'        => 'Can access the sites admin area',
-        'admin.settings'      => 'Can access the main site settings',
-        'users.manage-admins' => 'Can manage other admins',
-        'users.create'        => 'Can create new non-admin users',
-        'users.edit'          => 'Can edit existing non-admin users',
-        'users.delete'        => 'Can delete existing non-admin users',
-        'beta.access'         => 'Can access beta-level features',
+        'siswa.c_request' => 'Request penurunan poin siswa',
+        'siswa.access' => 'Request penurunan poin siswa',
+        'guru.c_pelanggaran' => 'Mencatat poin pelanggaran',
+        'guru.c_pengurangan' => 'Memproses request pengurangan',
+        'admin.c_pelanggaran' =>'Create pelanggaran poin',
+        'admin.u_pelanggaran' =>'Update pelanggaran poin',
+        'admin.d_pelanggaran' =>'Delete pelanggaran poin',
     ];
 
     /**
@@ -90,28 +82,8 @@ class AuthGroups extends ShieldAuthGroups
      * This defines group-level permissions.
      */
     public array $matrix = [
-        'superadmin' => [
-            'admin.*',
-            'users.*',
-            'beta.*',
-        ],
-        'admin' => [
-            'admin.access',
-            'users.create',
-            'users.edit',
-            'users.delete',
-            'beta.access',
-        ],
-        'developer' => [
-            'admin.access',
-            'admin.settings',
-            'users.create',
-            'users.edit',
-            'beta.access',
-        ],
-        'user' => [],
-        'beta' => [
-            'beta.access',
-        ],
+        'admin' => ['admin*', 'guru*', 'siswa.access'],
+        'siswa' => ['siswa*'],
+        'guru' => ['guru*', 'siswa.access']
     ];
 }
---
+++ app/Database/Seeds/UsersSeed.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UsersSeed extends Seeder
+{
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+    }
+}
+
+
+?>
